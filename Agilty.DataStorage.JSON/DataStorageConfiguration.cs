93429ef3b636cc478834b59a1fc898ac54f10f34
Igor-Ratsuk <igor.ratsuk@gmail.com>
2015-08-06 23:54:51 +0300

    #0 Core DataStorage objects

---
+++ Agilty.DataStorage.JSON/DataStorageConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace Agilty.DataStorage.JSON
+{
+    using System.Configuration;
+
+    #region Class: DataStorageConfiguration
+
+    public class DataStorageConfiguration: ConfigurationSection
+    {
+
+        #region Properties: Public
+
+        [ConfigurationProperty("DataFile")]
+        public string DataFile {
+            get { return (string)this["DataFile"]; }
+            set { this["DataFile"] = value; }
+        }
+
+        #endregion
+
+    }
+
+    #endregion
+
+}
---
+++ Agilty.DataStorage/DataStorageConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace Agilty.DataStorage
+{
+    using System.Configuration;
+
+    #region Class: DataStorageConfiguration
+
+
+    public class DataStorageConfiguration : ConfigurationSection
+    {
+
+        #region Properties: Public
+
+        [ConfigurationProperty("Provider")]
+        public string Provider
+        {
+            get { return (string)this["Provider"]; }
+            set { this["Provider"] = value; }
+        }
+
+        #endregion
+
+    }
+
+    #endregion
+
+}
